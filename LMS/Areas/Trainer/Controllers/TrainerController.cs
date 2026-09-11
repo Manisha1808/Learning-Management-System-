88505e70c0c0a9898f7bcc0e8f8055848025ed6d
Manisha1808 <manishasen320@gmail.com>
2026-09-11 16:28:24 +0530

    Change in Trainer

--- LMS/Areas/Trainer/Controllers/TrainerController.cs
+++ LMS/Areas/Trainer/Controllers/TrainerController.cs
@@ -3,38 +3,39 @@ using LMS.DB;
 using System.Data;
 using System.Linq;
 using System.Web.Mvc;
+using System.Collections.Generic;
 namespace LMS.Areas.Trainer.Controllers
 {
     public class TrainerController : Controller
-    {
-        protected override void OnActionExecuting(
-            ActionExecutingContext filterContext)
+    {  // This filter allows the user(admin) to always land in the Lgin Page first  
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             if (Session["UserRole"] == null ||
                 Session["UserRole"].ToString() != "3")
             {
-                filterContext.Result = RedirectToAction(
-                    "Login",
-                    "Account",
-                    new { area = "" }
-                );
+                filterContext.Result = RedirectToAction("Login","Account",new { area = "" });
                 return;
             }
             base.OnActionExecuting(filterContext);
         }
         private TrainerDB db = new TrainerDB();
-
         [HttpGet]
         public ActionResult Dashboard()
         {
+            ViewBag.Statuses = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "Not Started", Value = "Not Started" }, //Directly save the list here, rather than creating a seperate SP because there wont beany changes in this
+                new SelectListItem { Text = "In Progress", Value = "In Progress" },
+                new SelectListItem { Text = "Completed", Value = "Completed" }
+            };
             return View();
         }
+        //The employee data is fetched and displayed
         [HttpGet]
         public JsonResult GetEmployeeList(EmployeeProgress model)
-            {
+            {  
                 DataTable employeeList = db.GetEmployeeList(model);
-                var rows = employeeList.AsEnumerable()
-                    .Select(row => new
+                var rows = employeeList.AsEnumerable().Select(row => new
                     {
                         UserId = row["UserId"],
                         UserName = row["UserName"].ToString(),
@@ -45,12 +46,10 @@ namespace LMS.Areas.Trainer.Controllers
                     }).ToList();
                 return Json(
                     new
-                    {
-                        rows = rows
-                    },
+                    { rows = rows},
                     JsonRequestBehavior.AllowGet
                 );
             }
         }
-    }
+}
 
